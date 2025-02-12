289dca9d640a48ea24aaaddbeef9eb8ebb22101f
Kristo Kakkum <krkadf@taltech.ee>
2025-02-12 16:30:41 +0200

    Lisatud Komponent liides

---
+++ PrjTekstiPoorajaKomponent/ITeisendaja.vb
@@ -0,0 +1,9 @@
+﻿Public Interface ITeisendaja
+
+	Property EsimeseTaheAsciiKood As Integer ' Teksti esimese sümboli ASCII kood
+	Property ViimaseTaheAsciiKood As Integer ' Teksti viimase sümboli ASCII kood 
+	Property Tekst As String                 ' Kasutaja sisestatud tekst
+
+	Function PooraTekst() As String          ' Liidese meetodid koodi ei saa
+	Sub TeisendaTekst(ByRef sisendtekst As String)
+End Interface
