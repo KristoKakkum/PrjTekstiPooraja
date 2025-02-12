b97badf12c588a50571b50acf40124ed4be635bd
Kristo Kakkum <krkadf@taltech.ee>
2025-02-12 16:49:20 +0200

    TekstiPooraja

--- PrjTekstiPoorajaKomponent/TekstiPooraja.vb
+++ PrjTekstiPoorajaKomponent/TekstiPooraja.vb
@@ -1,3 +1,48 @@
 ﻿Public Class TekstiPooraja
+    Implements ITeisendaja
 
+    ' Klassi TekstiPooraja attribuudid (muutujad)
+    Private AlgusSymbol As Integer
+    Private LoppSymbol As Integer
+    Private PooratavTekst As String
+
+    ' Atribuut EsimeseThaeAsciikood seadistamine 
+    Private Property EsimeseTaheAsciiKood As Integer Implements ITeisendaja.EsimeseTaheAsciiKood
+        Get
+            Return AlgusSymbol      ' Atribuudi küsimisel tagastame AlgusSymbol väärtuse
+        End Get
+        Set(value As Integer)
+            AlgusSymbol = value     ' Salvestame atribuudi antava väärtuse
+        End Set
+    End Property
+
+    ' Atribuut ViimaseTaheAsciiKood seadistamine
+    Private Property ViimaseTaheAsciiKood As Integer Implements ITeisendaja.ViimaseTaheAsciiKood
+        Get
+            Return LoppSymbol       ' Atribuudi küsimisel tagastame LoppSymbol väärtuse
+        End Get
+        Set(value As Integer)
+            LoppSymbol = value      ' Salvestame atribuudile antava väärtuse
+        End Set
+    End Property
+
+    ' Atribuut tekst seadistamine
+    Private Property Tekst As String Implements ITeisendaja.Tekst
+        Get
+            Return PooratavTekst     ' Atribuudi küsimisel tagastame PooratavTekst väärtuse
+        End Get
+        Set(value As String)
+            PooratavTekst = value    ' Salvestame atribuudile antava väärtuse
+        End Set
+    End Property
+
+    ' Meetod võtab parameetrina edastatud sõna, pöörab
+    ' selle tagurpidi ning salvestab atribuuti Pooratavtekst
+    Private Sub TeisendaTekst(ByRef sisendtekst As String) Implements ITeisendaja.TeisendaTekst
+        PooratavTekst = StrReverse(sisendtekst)
+    End Sub
+
+    Private Function PooraTekst() As String Implements ITeisendaja.PooraTekst
+        Return StrReverse(PooratavTekst)
+    End Function
 End Class
